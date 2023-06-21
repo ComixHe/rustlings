e46fd8e6b9b863e77d5eb31f5956e12e346fd6eb
ComixHe <comixhe1895@outlook.com>
2023-06-22 02:17:30 +0800

    2023-06-22

--- exercises/iterators/iterators1.rs
+++ exercises/iterators/iterators1.rs
@@ -12,12 +12,12 @@
 fn main() {
     let my_fav_fruits = vec!["banana", "custard apple", "avocado", "peach", "raspberry"];
 
-    let mut my_iterable_fav_fruits = my_fav_fruits.iter_mut();
+    let mut my_iterable_fav_fruits = my_fav_fruits.iter();
 
-    assert_eq!(my_iterable_fav_fruits.next(), Some(&mut "banana"));
-    assert_eq!(my_iterable_fav_fruits.next(), Some(&mut "custard apple"));
-    assert_eq!(my_iterable_fav_fruits.next(), Some(&mut "avocado"));
-    assert_eq!(my_iterable_fav_fruits.next(), Some(&mut "peach"));
-    assert_eq!(my_iterable_fav_fruits.next(), Some(&mut "raspberry"));
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"banana"));
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"custard apple"));
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"avocado"));
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"peach"));
+    assert_eq!(my_iterable_fav_fruits.next(), Some(&"raspberry"));
     assert_eq!(my_iterable_fav_fruits.next(), None);
 }
