3d242c80539d8d12f8d8b044e96d63b10034e8f0
ComixHe <comixhe1895@outlook.com>
2023-06-23 10:10:48 +0800

    2023-06-23:Finish

--- exercises/conversions/try_from_into.rs
+++ exercises/conversions/try_from_into.rs
@@ -18,6 +18,16 @@ struct Color {
     blue: u8,
 }
 
+impl Color {
+    fn validate(num : i16) -> bool{
+        if num >= 0 && num <= 255{
+            true
+        } else {
+            false
+        }
+    }
+}
+
 // We will use this error type for these `TryFrom` conversions.
 #[derive(Debug, PartialEq)]
 enum IntoColorError {
@@ -26,8 +36,6 @@ enum IntoColorError {
     // Integer conversion error
     IntConversion,
 }
-
-// I AM NOT DONE
 
 // Your task is to complete this implementation and return an Ok result of inner
 // type Color. You need to create an implementation for a tuple of three
@@ -41,6 +49,11 @@ enum IntoColorError {
 impl TryFrom<(i16, i16, i16)> for Color {
     type Error = IntoColorError;
     fn try_from(tuple: (i16, i16, i16)) -> Result<Self, Self::Error> {
+        if Color::validate(tuple.0) && Color::validate(tuple.1) && Color::validate(tuple.2){
+            Ok(Color { red: tuple.0 as u8, green: tuple.1 as u8, blue: tuple.2 as u8})
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -48,6 +61,11 @@ impl TryFrom<(i16, i16, i16)> for Color {
 impl TryFrom<[i16; 3]> for Color {
     type Error = IntoColorError;
     fn try_from(arr: [i16; 3]) -> Result<Self, Self::Error> {
+        if Color::validate(arr[0]) && Color::validate(arr[1]) && Color::validate(arr[2]){
+            Ok(Color { red: arr[0] as u8, green: arr[1] as u8, blue: arr[2] as u8 })
+        } else {
+            Err(IntoColorError::IntConversion)
+        }
     }
 }
 
@@ -55,6 +73,15 @@ impl TryFrom<[i16; 3]> for Color {
 impl TryFrom<&[i16]> for Color {
     type Error = IntoColorError;
     fn try_from(slice: &[i16]) -> Result<Self, Self::Error> {
+        if slice.len() != 3{
+            Err(IntoColorError::BadLen)
+        } else {
+            if Color::validate(slice[0]) && Color::validate(slice[1]) && Color::validate(slice[2]){
+                Ok(Color { red: slice[0] as u8, green: slice[1] as u8, blue: slice[2] as u8 })
+            } else {
+                Err(IntoColorError::IntConversion)
+            }
+        }
     }
 }
 
